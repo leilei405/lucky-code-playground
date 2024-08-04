c7775a74b5e23939f386302e02122014d48fa0ab
leilei405 <1601178425@qq.com>
2024-08-04 17:07:50 +0800

    feat: 实现了编译以及在 iframe 里预览

--- src/components/Preview/index.tsx
+++ src/components/Preview/index.tsx
@@ -1,3 +1,49 @@
+import { useContext, useEffect, useState } from "react";
+import { compile } from "./compiler";
+import Editor from "../CodeEditor/Editor";
+import { PlaygroundContext } from "../../context";
+
+import iframeRaw from "./iframe.html?raw";
+import { IMPORT_MAP_FILE_NAME } from "../../pages//ReactPlayground/files";
+
 export default function Preview() {
-  return <div>Preview</div>;
+  const { files } = useContext(PlaygroundContext);
+  const [compiledCode, setCompiledCode] = useState("");
+  const getIframeUrl = () => {
+    const res = iframeRaw
+      .replace(
+        '<script type="importmap"></script>',
+        `<script type="importmap">${files[IMPORT_MAP_FILE_NAME].value}</script>`
+      )
+      .replace(
+        '<script type="module" id="appSrc"></script>',
+        `<script type="module" id="appSrc">${compiledCode}</script>`
+      );
+    return URL.createObjectURL(new Blob([res], { type: "text/html" }));
+  };
+
+  const [iframeUrl, setIframeUrl] = useState(getIframeUrl());
+
+  useEffect(() => {
+    const res = compile(files);
+    setCompiledCode(res);
+  }, [files]);
+
+  useEffect(() => {
+    setIframeUrl(getIframeUrl());
+  }, [files[IMPORT_MAP_FILE_NAME].value, compiledCode]);
+
+  return (
+    <div style={{ height: "100%" }}>
+      <iframe
+        src={iframeUrl}
+        style={{
+          width: "100%",
+          height: "100%",
+          padding: 0,
+          border: "none",
+        }}
+      />
+    </div>
+  );
 }
